7e005e09f191310782f52d6147251ee2fa9b78c1
Bahadir Ustabasi <oxikairos@hotmail.com>
2022-02-20 22:33:41 +0300

    updating our app with a shop page...

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 
 import './App.css';
 import React from 'react';
-import HomePage from '../src/homepage.component';
+import HomePage from '../src/pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+
+import {
+  BrowserRouter as Router,
+  Routes,                   // Switch = Routes
+  Route, 
+  Link
+} from 'react-router-dom'
+
 
 function App() {
   return (
     <div className="App">
-      <HomePage></HomePage>
+
+      {/* <Route path='/' component={HomePage}/>
+      <Route path='/hats' component={HatsPage}/> */}
+      
+      <Routes>
+
+        <Route path='/' element={<HomePage />}/>
+
+        <Route path='/shop' element={<ShopPage/>}/>
+
+      </Routes>
+
     </div>
   );
 }
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,38 @@
+
+import React from 'react';
+import SHOP_DATA from './shop.data';
+import CollectionPreview from '../../components/collection-preview/collection-preview';
+
+
+class ShopPage extends React.Component{
+    constructor(){
+        super();
+
+        this.state = {
+            collections: SHOP_DATA
+        }
+
+    }
+
+    render(){
+        const {collections} = this.state;
+        return(
+            
+            <div className='shop-page'>
+                <h1>SHOP PAGE</h1>
+                {collections.map((collection) => (
+                    <CollectionPreview
+                    key={collection.id}
+                    title={collection.title}
+                    items={collection.items}
+                    />
+                ))}
+
+            </div>
+          
+        );
+    }
+
+}
+
+export default ShopPage;
